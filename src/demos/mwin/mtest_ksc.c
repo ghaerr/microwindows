b377bc456e81a00ad1b57e444f42bf521eaabd92
greg <greg>
2001-07-04 19:54:42 +0000

    *** empty log message ***

---
+++ src/demos/mwin/mtest_ksc.c
@@ -0,0 +1,73 @@
+/* KSC5601 test program*/
+#include <windows.h>
+
+LRESULT CALLBACK wproc(HWND,UINT,WPARAM,LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   PSTR szCmdLine, int iCmdShow)
+{
+        static char szAppName[]="HolaWin";
+        HWND hwnd;
+        MSG msg;
+        WNDCLASS wndclass;
+
+        wndclass.style          = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+        wndclass.lpfnWndProc    = (WNDPROC)wproc;
+        wndclass.cbClsExtra     =0;
+        wndclass.cbWndExtra     =0;
+        wndclass.hInstance      =0;
+        wndclass.hIcon          =0;
+        wndclass.hCursor        =0;
+        wndclass.hbrBackground  =(HBRUSH)GetStockObject(LTGRAY_BRUSH);
+        wndclass.lpszMenuName   =NULL;
+        wndclass.lpszClassName  = szAppName;
+
+        RegisterClass(&wndclass);
+        hwnd=CreateWindowEx(0L,
+                          szAppName,
+                          "Hola",
+                          WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          200,
+                          200,
+                          NULL,
+                          NULL,
+                          NULL,
+                          NULL);
+               
+               
+        ShowWindow(hwnd,iCmdShow);
+        UpdateWindow(hwnd);
+        
+        while (GetMessage(&msg,NULL,0,0)) {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+        }      
+        return msg.wParam;
+}       
+LRESULT CALLBACK wproc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{       
+        HDC hdc;
+        PAINTSTRUCT ps;
+        RECT rect;
+        
+        switch (iMsg) {
+        case WM_CREATE:
+                break;
+        case WM_PAINT:
+        /*case WM_MOUSEFIRST:*/
+                hdc=BeginPaint(hwnd,&ps);
+                GetClientRect(hwnd,&rect);
+                DrawText(hdc, "Hola, NOS ¤¾¤¿¤¤ÇÑ", -1, &rect,
+                         DT_SINGLELINE|DT_CENTER|DT_VCENTER);
+                EndPaint(hwnd,&ps);
+                break;
+        case WM_DESTROY:
+                PostQuitMessage(0);
+                break;
+        default:
+                return DefWindowProc(hwnd,iMsg,wParam,lParam);
+        }      
+        return (0);
+}
---
+++ src/demos/nanox/logfont_ksc.c
@@ -0,0 +1,108 @@
+/* KSC5601 test program*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#if UNIX
+#include <time.h>
+#endif
+#define MWINCLUDECOLORS
+#include "nano-X.h"
+/*
+ * logical font demo for Nano-X
+ */
+
+#define MAXW 		630
+#define MAXH 		470
+
+int main(int argc, char **argv)
+{
+	GR_WINDOW_ID 	window;
+	GR_EVENT 	event;
+        GR_GC_ID 	gc;
+	GR_FONT_ID	fontid;
+        int 		x, y, rnd = 0;
+	MWLOGFONT	lf;
+	char		description[128];
+   
+        srand(time(0));
+   
+        GrOpen();
+	window = GrNewWindow(GR_ROOT_WINDOW_ID, 5, 5, MAXW, MAXH, 4, BLACK, BLUE);
+	GrMapWindow(window);
+
+        gc = GrNewGC();
+
+        GrSelectEvents(window,GR_EVENT_MASK_ALL);
+        GrSetGCUseBackground(gc,GR_FALSE);
+	GrSetGCBackground(gc, GR_RGB(0, 0, 0));
+
+	y = 30;
+	x = 0;
+
+	while(1) {
+	      GrCheckNextEvent(&event);
+
+	      if(event.type == GR_EVENT_TYPE_CLOSE_REQ) {
+			GrClose();
+			exit(0);
+	      }
+
+	      sleep(1);
+
+	      MWLF_Clear(&lf);
+	      description[0] = '\0';
+
+	      // lf.lfSerif = 1;
+
+	      if ( rnd & 1 ) {
+		      lf.lfWeight = MWLF_WEIGHT_BOLD;
+		      strcat(description,"Bold ");
+	      }
+	      
+
+	      if ( rnd & 2 ) {
+		      lf.lfItalic = 1;
+		      strcat(description,"Italics ");
+	      }
+	      if ( rnd & 4 ) {
+		      lf.lfOblique = 1;
+		      strcat(description,"Oblique ");
+	      }
+
+	      if ( rnd & 8 ) {
+		      lf.lfMonospace = 1;
+		      strcat(description,"°íÁ¤Æø Monospace ");
+	      } else {
+		      lf.lfProportional = 1;
+		      strcat(description,"Proportional ");
+	      }
+
+	      if ( argc > 1 )
+		      strcpy(lf.lfFaceName,argv[1]);
+	      else
+		      strcpy(lf.lfFaceName,"fantasy");
+
+	      fontid = GrCreateFont(0, 0, &lf);
+	      /* GrSetFontSize(fontid, 1+(int)(80.0 * rand() / (RAND_MAX+1.0))); */
+	      GrSetFontSize(fontid,26);
+	      GrSetFontRotation(fontid, 330);	/* 33 degrees*/
+  	      GrSetFontAttr(fontid, GR_TFKERNING | GR_TFANTIALIAS, 0);
+  	      GrSetGCFont(gc, fontid);
+	      /*GrSetGCBackground(gc, rand() & 0xffffff);*/
+ 	      GrSetGCForeground(gc, 0xffffff);
+	      /* x = (int) ((MAXW * 1.0) *rand()/(RAND_MAX+1.0));
+		 y = (int) ((MAXH * 1.0) *rand()/(RAND_MAX+1.0)); */
+
+	      GrText(window, gc,x,y, description, -1, GR_TFASCII);
+
+	      GrDestroyFont(fontid);
+
+	      rnd++;
+	      y += 30;
+	      if ( y > 460 )
+		      y = 0;
+	}
+
+	GrClose();
+}

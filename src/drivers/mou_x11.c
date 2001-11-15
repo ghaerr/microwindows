ffa4c597a1e9926d38ae06a00f6534dca24b2cde
greg <greg>
2001-11-15 23:58:45 +0000

    *** empty log message ***

--- src/drivers/mou_x11.c
+++ src/drivers/mou_x11.c
@@ -123,8 +123,6 @@ X11_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp)
 			button = MWBUTTON_M;
 		else if(ev.xbutton.button == 3)
 			button = MWBUTTON_R;
-		else
-			button = 0;
 
 		/* Get any other buttons that might be already held */
 		if (ev.xbutton.state & Button1Mask)
@@ -134,8 +132,8 @@ X11_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp)
 		if (ev.xbutton.state & Button3Mask)
 		    button |= MWBUTTON_R;
 		
-//		printf("!Pressing button: 0x%x, state: 0x%x, button: 0x%x\n",
-//		button,ev.xbutton.state, ev.xbutton.button);
+/*		printf("!Pressing button: 0x%x, state: 0x%x, button: 0x%x\n",
+			button,ev.xbutton.state, ev.xbutton.button);*/
 		*bp = button;
 		*dx = ev.xbutton.x;
 		*dy = ev.xbutton.y;
@@ -146,17 +144,16 @@ X11_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp)
 	else if (ev.type == ButtonRelease) {
 	    if (ev.xbutton.window == x11_win) {
 	        int button = 0;
-		
-		/* Get pressed button */
+		int released = 0;
+	
+		/* Get released button */
 	    	if(ev.xbutton.button == 1)
-			button = MWBUTTON_L;
+			released = MWBUTTON_L;
 		else if(ev.xbutton.button == 2)
-			button = MWBUTTON_M;
+			released = MWBUTTON_M;
 		else if(ev.xbutton.button == 3)
-			button = MWBUTTON_R;
-		else
-			button = 0;
-
+			released = MWBUTTON_R;
+		
 		/* Get any other buttons that might be already held */
 		if (ev.xbutton.state & Button1Mask)
 		    button |= MWBUTTON_L;
@@ -164,9 +161,13 @@ X11_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp)
 		    button |= MWBUTTON_M;
 		if (ev.xbutton.state & Button3Mask)
 		    button |= MWBUTTON_R;
-		
-//		printf("!Releasing button: 0x%x, state: 0x%x, button: 0x%x\n",
-//		button,ev.xbutton.state, ev.xbutton.button);
+	
+		/* We need to remove the released button from the button mask*/
+		button &= ~released;
+
+		/*printf("!Releasing button: 0x%x, state: 0x%x, button: 0x%x\n",
+			button,ev.xbutton.state, ev.xbutton.button);*/
+
 		*bp = button;
 		*dx = ev.xbutton.x;
 		*dy = ev.xbutton.y;

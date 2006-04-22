d603caaeaff671dc8133de212e5387341f53a0b6
greg <greg>
2006-04-22 01:05:04 +0000

    *** empty log message ***

--- src/engine/devimage_stretch.c
+++ src/engine/devimage_stretch.c
@@ -1,31 +1,13 @@
 /*
- * Copyright (c) 2000, 2001, 2003 Greg Haerr <greg@censoft.com>
+ * Copyright (c) 2000, 2001, 2003, 2006 Greg Haerr <greg@censoft.com>
  *
  * StretchImage - Resize an image
- *
- * !!! WARNING Not under the standard Microwindows license !!!
- * !!!         Read the following license (GPL)            !!!
  *
  * Major portions from SDL Simple DirectMedia Layer by Sam Lantinga
  * Copyright (C) 1997, 1998, 1999, 2000  Sam Lantinga <slouken@devolution.com>
  * This a stretch blit implementation based on ideas given to me by
  * Tomasz Cejner - thanks! :)
  */
-/*
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

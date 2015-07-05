86c0547a886577f789c9c5c802b28a55391d2549
georgp24 <nospam@georgpotthast.de>
2015-07-05 07:41:06 +0200

    Replace printf with DPRINTF in nxlib/Text16.c - credits: Ian MacArthur

--- src/nxlib/Text16.c
+++ src/nxlib/Text16.c
@@ -4,7 +4,7 @@ int
 XDrawString16(Display *dpy, Drawable d, GC gc, int x, int y,
 	_Xconst XChar2b *string, int length)
 {   
-printf("XDrawString16 %d %x %x\n", length, string->byte1, string->byte2);
+DPRINTF("XDrawString16 %d %x %x\n", length, string->byte1, string->byte2);
 	if (length > 0)
 		GrText(d, gc->gid, x, y, (void *)string, length, 
 		       GR_TFXCHAR2B|GR_TFBASELINE);

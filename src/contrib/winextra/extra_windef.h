79aaeac40c9fb4b2a3932af3eb42365de392cdef
roozbeh@homewin10.com <roozbeh@homewin10.com>
2019-02-05 02:45:47 -0700

    Required to build mwin and nx11

--- src/contrib/winextra/extra_windef.h
+++ src/contrib/winextra/extra_windef.h
@@ -45,12 +45,6 @@ typedef struct _ULARGE_INTEGER {
 	ULONGLONG QuadPart;
 } ULARGE_INTEGER;
 
-#if _MSC_VER
-struct timeval {
-	long tv_sec;
-	long tv_usec;
-};
-#endif
 
 typedef struct timeval TIMEVAL, *PTIMEVAL, *LPTIMEVAL;
 
---
+++ src/contrib/winextra/sys/time.h
@@ -0,0 +1,6 @@
+#if _MSC_VER
+struct timeval {
+	long tv_sec;
+	long tv_usec;
+};
+#endif

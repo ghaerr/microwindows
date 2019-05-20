bcd3f8835ce1d0ac560f59d827d3184acefb74bc
Greg Haerr <greg@censoft.com>
2019-05-20 15:40:29 -0700

    cleanup demos/nanox/test-unmap.c demo program showing unmap/map without event wait

--- src/demos/nanox/test-unmap.c
+++ src/demos/nanox/test-unmap.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "nano-X.h"
-
+/*
+ * Demo program showing workaround for unmap/map without event wait
+ */
 int
 main(int ac,char **av)
 {
@@ -17,18 +19,16 @@ main(int ac,char **av)
 		return 1;
 	}
 
-	//w = GrNewWindowEx(GR_WM_PROPS_APPWINDOW|GR_WM_PROPS_NOBACKGROUND, "Nano-X Demo2",
-		//GR_ROOT_WINDOW_ID, 20, 20, 320, 240, BLACK);
-	w = GrNewWindowEx(GR_WM_PROPS_BORDER|GR_WM_PROPS_NOBACKGROUND, "Nano-X Demo2",
+	w = GrNewWindowEx(GR_WM_PROPS_APPWINDOW|GR_WM_PROPS_NOBACKGROUND, "Nano-X Demo2",
 		GR_ROOT_WINDOW_ID, 20, 20, 320, 240, BLACK);
 
 	gc = GrNewGC();
-	//font = GrCreateFontEx("lubI24.pcf", 0, 0, NULL);
 	font = GrCreateFontEx("helvB12.pcf.gz", 0, 0, NULL);
 	GrSetGCFont(gc, font);
 
+	GrSelectEvents(GR_ROOT_WINDOW_ID, GR_EVENT_MASK_BUTTON_DOWN | GR_EVENT_MASK_BUTTON_UP);
 	GrSelectEvents(w, GR_EVENT_MASK_EXPOSURE | GR_EVENT_MASK_CLOSE_REQ
-		| GR_EVENT_MASK_BUTTON_DOWN | GR_EVENT_MASK_KEY_DOWN | GR_EVENT_MASK_KEY_UP);
+		| GR_EVENT_MASK_KEY_DOWN | GR_EVENT_MASK_KEY_UP);
 	GrMapWindow(w);
 	GrSetFocus(w);
 
@@ -39,7 +39,6 @@ main(int ac,char **av)
 		GR_EVENT 	event;
 
 		GrGetNextEvent(&event);
-
 		switch (event.type) {
 		case GR_EVENT_TYPE_EXPOSURE:
 			GrSetGCForeground(gc,GrGetSysColor(GR_COLOR_APPWINDOW));
@@ -50,24 +49,24 @@ main(int ac,char **av)
 			GrText(w, gc, 10, 30, "Hello World", -1, GR_TFASCII);
             GrRect(w, gc, 5, 5, 300, 60);
 			GrGetWindowInfo(w, &wi);
-			GrError("Exposure:wi.width:%d,wi.height:%d,wi.x:%d,wi.y:%d,wi.parent:%d\n",wi.width,wi.height,wi.x,wi.y,wi.parent);
 			break;
 
 		case GR_EVENT_TYPE_BUTTON_DOWN:
-			// FIXME unmap window is broken
 			GrUnmapWindow(w);
-			GrFlush();
+
+			/* GrCheckNextEvent is REQUIRED to have nanowm inspect previous unmap
+			 * before next map request, or server bug and no map results.
+			 */
+			GrCheckNextEvent(&event);
+printf("possible loss of event type %d\n", event.type);
 			GrMapWindow(w);
-
-			//uncomment to test server error on bad syscall
-			//GrMoveWindow(GR_ROOT_WINDOW_ID, 0, 0);
-
-			GrGetWindowInfo(w, &wi);
-			GrError("Button:  wi.width:%d,wi.height:%d,wi.x:%d,wi.y:%d,wi.parent:%d\n",wi.width,wi.height,wi.x,wi.y,wi.parent);
+			break;
+		case GR_EVENT_TYPE_BUTTON_UP:
+			//GrMapWindow(w);
 			break;
 
 		case GR_EVENT_TYPE_ERROR:
-			GrError("demo2: Error (%s) ", event.error.name);
+			GrError("test-unmap: Error (%s) ", event.error.name);
 			GrError(nxErrorStrings[event.error.code], event.error.id);
 			break;
 

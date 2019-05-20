ad3164ddd42fb28761a04ec69534e4486a5894f8
Greg Haerr <greg@censoft.com>
2019-05-20 16:54:07 -0700

    add scrollwheel handling to DEVMICEMOUSE Linux mouse driver drivers/mou_devmice.c

--- src/drivers/mou_devmice.c
+++ src/drivers/mou_devmice.c
@@ -2,6 +2,7 @@
  * Copyright (c) 1999 Greg Haerr <greg@censoft.com>
  *
  * Kurt Nalty 2019 
+ *	5/20/19 added scrollwheel support <greg@censoft.com>
  *
  * /dev/input/mice Mouse Driver
  */
@@ -21,9 +22,11 @@ static void 	Mice_Close(void);
 static int  	Mice_GetButtonInfo(void);
 static void	Mice_GetDefaultAccel(int *pscale,int *pthresh);
 static int  	Mice_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp);
-static int  	Mice_Poll(void);
 
 static int      mouse_fd = -1;
+
+/* sequence to mouse device to send ImPS/2 events*/
+static const unsigned char imps2[] = { 0xf3, 200, 0xf3, 100, 0xf3, 80 };
 
 /* Interface to Mouse Device Driver*/
 // (from device.h)
@@ -51,9 +54,8 @@ MOUSEDEVICE mousedev = {
 	Mice_GetButtonInfo,
 	Mice_GetDefaultAccel,
 	Mice_Read,
-//	Mice_Poll
-        NULL,
-        MOUSE_NORMAL    /* flags*/
+	NULL,
+	MOUSE_NORMAL    /* flags*/
 };
 
 /////////////////////////////////////////////
@@ -65,12 +67,21 @@ MOUSEDEVICE mousedev = {
 static int
 Mice_Open(MOUSEDEVICE *pmd)
 {
-        mouse_fd = open(MICE_DEV_FILE, O_RDONLY | O_NONBLOCK);
+	unsigned char buf[4];
 
-        if (mouse_fd < 0)
-			return MOUSE_FAIL;
+	mouse_fd = open(MICE_DEV_FILE, O_RDONLY | O_NONBLOCK);
 
-        return mouse_fd;
+#if 1
+    /* switch the mouse to ImPS/2 protocol*/
+	if (write(mouse_fd, imps2, sizeof(imps2)) != sizeof(imps2))
+		DPRINTF("Can't switch to ImPS/2 protocol\n");
+	if (read(mouse_fd, buf, 4) != 1 || buf[0] != 0xF4)
+		DPRINTF("Failed to switch to ImPS/2 protocol.\n");
+#endif
+	if (mouse_fd < 0)
+		return MOUSE_FAIL;
+
+	return mouse_fd;
 }
 
 /////////////////////////////////////////////
@@ -81,9 +92,9 @@ Mice_Open(MOUSEDEVICE *pmd)
 static void
 Mice_Close(void)
 {
-        if (mouse_fd >= 0)
-                close(mouse_fd);
-        mouse_fd = -1;
+	if (mouse_fd >= 0)
+		close(mouse_fd);
+	mouse_fd = -1;
 }
 
 /////////////////////////////////////////////
@@ -94,7 +105,7 @@ Mice_Close(void)
 static int
 Mice_GetButtonInfo(void)
 {
-        return MWBUTTON_L | MWBUTTON_M | MWBUTTON_R;
+	return MWBUTTON_L | MWBUTTON_M | MWBUTTON_R | MWBUTTON_SCROLLUP | MWBUTTON_SCROLLDN;
 }
 
 /////////////////////////////////////////////
@@ -112,34 +123,40 @@ Mice_GetDefaultAccel(int *pscale,int *pthresh)
 /////////////////////////////////////////////
 
 
-/*
-	O_UNBLOCK reads of /dev/input/mice  routinely returns -1 when no
-        new data is available. When new data is available, we read three
-	bytes: 
-		data[0] is mouse button status,
-			bit 2 middle, bit 1 right, bit 0 left
-		data[1] is delta x (signed byte)
-		data[2] is delta y (signed byte)
-
-*/
-
+/* IntelliMouse PS/2 protocol uses four byte reports
+ * (PS/2 protocol omits last byte):
+ *      Bit   7     6     5     4     3     2     1     0
+ * --------+-----+-----+-----+-----+-----+-----+-----+-----
+ *  Byte 0 |  0     0   Neg-Y Neg-X   1    Mid  Right Left
+ *  Byte 1 |  X     X     X     X     X     X     X     X
+ *  Byte 2 |  Y     Y     Y     Y     Y     Y     Y     Y
+ *  Byte 3 |  W     W     W     W     W     W     W     W
+ *
+ * XXXXXXXX, YYYYYYYY, and WWWWWWWW are 8-bit two's complement values
+ * indicating changes in x-coordinate, y-coordinate, and scroll wheel.
+ * That is, 0 = no change, 1..127 = positive change +1 to +127,
+ * and 129..255 = negative change -127 to -1.
+ *
+ * Left, Right, and Mid are the three button states, 1 if being depressed.
+ * Neg-X and Neg-Y are set if XXXXXXXX and YYYYYYYY are negative, respectively.
+ */
 static int
 Mice_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp)
 {
 	unsigned char data[4];
 	int Bytes_Read;
-        int left, middle, right, button = 0;
-        signed char x, y, z;
+	int left, middle, right, button = 0;
+	signed char x, y, w;
 
-// Read Mouse. Ask for four bytes, expect three.
+// Read Mouse. Ask for four bytes, expect three or four.
 
-        Bytes_Read = read(mouse_fd, data, sizeof(data));
-	if (Bytes_Read != 3)
+	Bytes_Read = read(mouse_fd, data, sizeof(data));
+	if (Bytes_Read != 3 && Bytes_Read != 4)
 		return MOUSE_NODATA;
 
-        left = data[0] & 0x1;
-        right = data[0] & 0x2;
-        middle = data[0] & 0x4;
+	left = data[0] & 0x1;
+	right = data[0] & 0x2;
+	middle = data[0] & 0x4;
 
 // remap these bits to Nano-X expected format
 
@@ -147,30 +164,25 @@ Mice_Read(MWCOORD *dx, MWCOORD *dy, MWCOORD *dz, int *bp)
 	if (middle)	button |= MWBUTTON_M;
 	if (right)	button |= MWBUTTON_R;
 
-	*bp = button;
 
 // mwtypes.h:268:typedef int    MWCOORD;        // device coordinates
 
-        x =   (signed char) data[1];
-        y = - (signed char) data[2];  // y axis flipped between conventions
-        z =   0;
+	x =   (signed char) data[1];
+	y = - (signed char) data[2];  // y axis flipped between conventions
+	if (Bytes_Read == 4) {
+        w = (signed char) data[3];
+		if (w > 0)
+			button |= MWBUTTON_SCROLLUP;
+		if (w < 0)
+			button |= MWBUTTON_SCROLLDN;
+	}
+
 	*dx = x;	// integer from signed char
 	*dy = y;       
-	*dz = z;
+	*dz = 0;
+	*bp = button;
 
 	return MOUSE_RELPOS;
-}
-
-/////////////////////////////////////////////
-
-/*
- * Poll for events
- */
-
-static int
-Mice_Poll(void)
-{
-  return 0;
 }
 
 /////////////////////////////////////////////
